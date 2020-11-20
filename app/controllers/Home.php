f9a53a3c24b1751773f44f016f3a774a0094410e
Iuri Albuquerque Reis <iareis@gmail.com>
2020-11-20 01:50:37 -0300

    app/controllers/Home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,17 +1,22 @@
 <?php
-class Home extends Controller {
 
+use Controller;
+
+class Home extends Controller
+{
     // Index of the home page (localhost/home(/index))
-    public function index($param1= '', $param2= '', $param3= '') {
-        
+    public function index($param1 = '', $param2 = '', $param3 = '')
+    {
         // Initialize Alunos model
         $test = $this->model('Alunos');
 
         // Call function from the model
         $testData = $test->getAlunosFunction();
-      
 
-        $this->view('home/index', ['test' => $testData, 'parameters' => [$param1, $param2, $param3]]);
+        $this->view('home/index', [
+            'test' => $testData,
+            'parameters' => [$param1, $param2, $param3],
+        ]);
     }
 }
 
